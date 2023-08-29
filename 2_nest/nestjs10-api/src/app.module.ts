09486745a2918dfbfd17b699e7061b77317018dd
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-29 14:26:38 -0300

    categories and prisma config

--- 2_nest/nestjs10-api/src/app.module.ts
+++ 2_nest/nestjs10-api/src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CategoriesModule } from './categories/categories.module'
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), CategoriesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
