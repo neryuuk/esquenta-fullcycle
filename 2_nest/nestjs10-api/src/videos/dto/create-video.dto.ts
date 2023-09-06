0fc6f42dd8ed6aaf7ec17f8ca9bd5cdd83717f1f
Nelson Antunes <966712+neryuuk@users.noreply.github.com>
2023-09-06 15:09:27 -0300

    video fields decorators

--- 2_nest/nestjs10-api/src/videos/dto/create-video.dto.ts
+++ 2_nest/nestjs10-api/src/videos/dto/create-video.dto.ts
@@ -1,1 +1,20 @@
-export class CreateVideoDto {}
+import { IsNotEmpty, IsString, MaxLength, isInt } from 'class-validator'
+
+export class CreateVideoDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  title: string
+
+  @IsOptional()
+  @IsString()
+  description?: string | null
+
+  @IsNotEmpty()
+  @IsString()
+  file_path: string
+
+  @isInt()
+  @IsNotEmpty()
+  category_id: int
+}
