795e5f486371adbd791d5a50ee1626a2997f7bed
Nelson Antunes <966712+neryuuk@users.noreply.github.com>
2024-03-31 13:34:40 -0300

    custom logger

--- 2_nest/nestjs10-api/src/main.ts
+++ 2_nest/nestjs10-api/src/main.ts
@@ -5,9 +5,13 @@ import { PrismaExceptionFilter } from '@categories/exception-filters/prisma.exce
 import { CatchAllExceptionFilter } from '@categories/exception-filters/catch-all.exception-filter'
 import { InvalidRelationExceptionFilter } from '@categories/exception-filters/invalid-relation.exception-filter'
 import { IllegalCharactersExceptionFilter } from '@videos/exception-filters/illegal-characters.exception-filter'
+import { CustomLogger } from './custom.logger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    forceCloseConnections: true,
+    logger: new CustomLogger(),
+  })
 
   app.useGlobalFilters(
     new CatchAllExceptionFilter(app.get(HttpAdapterHost).httpAdapter),
